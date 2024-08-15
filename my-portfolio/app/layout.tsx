742d698f88b6ee6306fa1d68185cd05f93cfb183
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-08-15 14:57:53 +0530

    afdeghgh

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -9,15 +9,12 @@ import { Toaster } from "@/components/ui/toaster"
 import Footer from "@/components/Footer";
 import BackToTopButton from "@/components/BackToTopButton";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
 import { cn } from "@/lib/utils"
 const fontSans = FontSans({
   subsets: ["latin"],
-  variable: "--font-sans", 
+  variable: "--font-sans",
 })
-
-// const inter = Inter({ subsets: ["latin"] });
-
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Harshil Sharma | Portfolio",
   description: "Harshil Sharma - Portfolio",
